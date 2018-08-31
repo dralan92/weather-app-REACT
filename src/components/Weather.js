338dc4c124edad7f541846affbd70332baac2e9d
unknown <codename100.alan@gmail.com>
2018-08-31 03:41:22 -0400

    Parsed data to be displayed as props of Weather Component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component{
     render(){
         return(
             <div>
-                Weather Component
+                { this.props.city}
+                { this.props.country}
+                { this.props.temperature}
+                { this.props.humudity}
+                { this.props.description}
             </div>
         );
     }

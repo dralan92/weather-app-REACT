406036fa67f0f299e1a371cf3171aa3373f40812
unknown <codename100.alan@gmail.com>
2018-08-31 03:55:42 -0400

    Displayed parsed data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,11 +4,10 @@ class Weather extends React.Component{
     render(){
         return(
             <div>
-                { this.props.city}
-                { this.props.country}
-                { this.props.temperature}
-                { this.props.humudity}
-                { this.props.description}
+                {this.props.city && this.props.country && <p>Location: { this.props.city},{ this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: { this.props.temperature}</p>}
+                { this.props.humudity && <p>Humidity: { this.props.humudity}</p>}
+                {this.props.description && <p>Condition: { this.props.description}</p>}
             </div>
         );
     }

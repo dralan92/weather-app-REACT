913e0588b1de6ec159b04b59a9fb8cad57c6712e
unknown <codename100.alan@gmail.com>
2018-08-31 11:11:23 -0400

    Added bootstrap and css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Weather = (props)=>{
     return(
-    <div>
-        {props.city && props.country && <p>Location: { props.city},{ props.country}</p>}
-        {props.temperature && <p>Temperature: { props.temperature}</p>}
-        {props.humidity && <p>Humidity: { props.humidity}</p>}
-        {props.description && <p>Condition: { props.description}</p>}
-        {props.error && <p>{props.error}</p>}
+    <div className = "weather_info" >
+        {props.city && props.country && <p className = "weather__key">Location: { props.city},{ props.country}</p>}
+        {props.temperature && <p className = "weather__key">Temperature: { props.temperature}</p>}
+        {props.humidity && <p className = "weather__key">Humidity: { props.humidity}</p>}
+        {props.description && <p className = "weather__key" >Condition: { props.description}</p>}
+        {props.error && <p className = "weather__key" >{props.error}</p>}
     </div>
     );
 }

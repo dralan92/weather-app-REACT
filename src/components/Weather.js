3751ff3c235339cf598723f5731d72875242e4bc
unknown <codename100.alan@gmail.com>
2018-08-31 04:00:07 -0400

    humidity spelling corrected

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,7 @@ class Weather extends React.Component{
             <div>
                 {this.props.city && this.props.country && <p>Location: { this.props.city},{ this.props.country}</p>}
                 {this.props.temperature && <p>Temperature: { this.props.temperature}</p>}
-                { this.props.humudity && <p>Humidity: { this.props.humudity}</p>}
+                {this.props.humidity && <p>Humidity: { this.props.humidity}</p>}
                 {this.props.description && <p>Condition: { this.props.description}</p>}
             </div>
         );
